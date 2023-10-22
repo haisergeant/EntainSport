e98855b1068b167d5c06f1d61b8ebc0dc483adee
Hai Le <haisergeant@gmail.com>
2023-10-22 22:28:54 +1100

    Initial Commit

---
+++ EntainSport/EntainSportApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntainSportApp.swift
+//  EntainSport
+//
+//  Created by Hai Le on 22/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EntainSportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
